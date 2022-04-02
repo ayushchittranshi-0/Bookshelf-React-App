f65ebd7842096b65fab224c258cd354a69b41ac1
“ayushchittranshi-0” <ayushchittranshi.0@gmail.com>
2022-04-02 19:13:22 +0530

    2nd commit

--- src/Components/SearchBooks.js
+++ src/Components/SearchBooks.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 const SearchBooks = () => {
-  const searchBookHandler = () => {};
+  const [bookInput, setBookInput] = useState("Ayush");
+
+  const GOOGLE_API_KEY = "AIzaSyBN4j6yCYLRBWpikfZ-sK-_SlF72wgH_Ns";
+  const BOOK_API = "https://www.googleapis.com/books/v1/volumes?q=";
+
+  const inputSearchHandler = (e) => {
+    setBookInput(e.target.value);
+  };
+
+  const searchClickHandler = async () => {
+    const response = await fetch(
+      `${BOOK_API}${bookInput}&key=${GOOGLE_API_KEY}`
+    );
+
+    console.log(`${BOOK_API}${bookInput}&key=${GOOGLE_API_KEY}`);
+
+    const data = await response.json();
+    console.log(data);
+  };
 
   return (
     <div>
-      <input type="text" placeholder="Search Books"></input>
-      <button onClick={searchBookHandler}>Search</button>
+      <input
+        type="text"
+        placeholder="Search Books"
+        onChange={inputSearchHandler}
+        value={bookInput}
+      ></input>
+      <button onClick={searchClickHandler}>Search</button>
     </div>
   );
 };
